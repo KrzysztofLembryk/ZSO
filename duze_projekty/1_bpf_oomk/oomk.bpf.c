17046354c45d5aa4172b6f9ee12326578f9676ff
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2026-03-21 11:58:48 +0100

    getting ram usage works

--- duze_projekty/1_bpf_oomk/oomk.bpf.c
+++ duze_projekty/1_bpf_oomk/oomk.bpf.c
@@ -24,8 +24,8 @@ struct {
     // We use LRU map since if there is no free space left in our map it will remove
     // least used entry from it, and because we need to pre-allocate our map it will 
     // have fixed size, so such mechanism is useful.
-    __uint(type, BPF_MAP_TYPE_LRU_HASH); 
-    __uint(max_entries, MAX_ENTRIES);
+    __uint(type, BPF_MAP_TYPE_HASH); 
+    __uint(max_entries, 1);
     __type(key, __u32);
     __type(value, __u64);
 } sysinfo_map SEC(".maps");
@@ -36,29 +36,30 @@ struct {
 SEC("kprobe/si_meminfo")
 int check_ram_usage_entry(void *ctx)
 {
-    struct task_struct *task = (void *)bpf_get_current_task();
+    // In sysinfo we have freeram, totalram and MEM_UNIT, since there is a MEM_UNIT
+    // it means that freeram and totalram are not in bytes, but in mem_units, thus
+    // to get sizes in bytes we need to multiply by mem_unit
     struct sysinfo *sys_info = (struct sysinfo *)PT_REGS_PARM1((struct pt_regs*)ctx);
 
-    __u16 n_proc = BPF_CORE_READ(sys_info, procs); 
-    __u64 free_ram_addr = (__u64)&(sys_info->freeram);
-    __u64 total_ram_addr = (__u64)&(sys_info->totalram);
-    // pid_t pid;
-    // pid = bpf_get_current_pid_tgid() >> 32;
+    // We save our pointer to sys_info struct in our map, so that we can access it 
+    // in KRETPROBE, since currently this struct is uninitialized. 
+    // In kretprobe's ctx we cannot access this struct, we can only get return value,
+    // so we will store pointer to it in our map.
+    __u64 sys_info_addr = (__u64)sys_info;
+    pid_t pid = bpf_get_current_pid_tgid() >> 32;
 
-    __u32 key = 0;
+    // We save addresses to our variables in map so that in KRETPROBE we can access
+    // them, since currently they are uninitialized, however in kretprobe's ctx we 
+    // don't get these arguments, we can only get return value, thus we store them
+    // in our map
     bpf_map_update_elem(
         &sysinfo_map, 
-        &key, 
-        &free_ram_addr,
-        BPF_ANY
-    );
-
-    key = 1;
-    bpf_map_update_elem(
-        &sysinfo_map, 
-        &key, 
-        &total_ram_addr,
-        BPF_ANY
+        &pid, 
+        // &sys_info since map_update looks at what is inside the pointer
+        // and copies these values, so the result is the same as if we would pass
+        // &sys_info_addr
+        &sys_info_addr, 
+        BPF_NOEXIST
     );
 
     return 0;
@@ -70,41 +71,27 @@ int check_ram_usage_entry(void *ctx)
 SEC("kretprobe/si_meminfo")
 int check_ram_usage_exit(void *ctx)
 {
-    struct task_struct *task = (void *)bpf_get_current_task();
+    // struct task_struct *task = (void *)bpf_get_current_task();
+    pid_t pid = bpf_get_current_pid_tgid() >> 32;
+    __u64 *sys_info_ptr = bpf_map_lookup_elem(&sysinfo_map, &pid);
 
-    // pid_t pid;
-    // pid = bpf_get_current_pid_tgid() >> 32;
-    __u32 key = 0;
-    __u64 *free_ram_addr_ptr = bpf_map_lookup_elem(&sysinfo_map, &key);
-
-    if (!free_ram_addr_ptr)
+    if (!sys_info_ptr)
     {
-        bpf_printk("free_ram_addr is null");
         return 0;
     }
 
-    __u64 free_ram_addr = *free_ram_addr_ptr;
-    __u64 free_ram_val = 0;
+    struct sysinfo *sys_info = (struct sysinfo *)*sys_info_ptr; 
 
-    bpf_probe_read_kernel((void*)&free_ram_val, sizeof(free_ram_val), (void*)free_ram_addr);
+    __u64 free_ram; 
+    __u64 total_ram; 
+    __u32 mem_unit;
+    bpf_probe_read_kernel(&free_ram,  sizeof(free_ram),  &sys_info->freeram);
+    bpf_probe_read_kernel(&total_ram, sizeof(total_ram),  &sys_info->totalram);
+    bpf_probe_read_kernel(&mem_unit,  sizeof(mem_unit),  &sys_info->mem_unit);
 
+    bpf_printk("\nFree Ram: %lu \nTotal Ram: %lu\nMem_unit: %u", free_ram * mem_unit, total_ram * mem_unit, mem_unit);
 
-    key = 1;
-    __u64 *total_ram_addr_ptr = bpf_map_lookup_elem(&sysinfo_map, &key);
-
-    if (!total_ram_addr_ptr)
-    {
-        bpf_printk("free_ram_addr is null");
-        return 0;
-    }
-
-    __u64 total_ram_addr = *total_ram_addr_ptr;
-    __u64 total_ram_val = 0;
-
-    bpf_probe_read_kernel((void*)&total_ram_val, sizeof(total_ram_val), (void*)total_ram_addr);
-
-    bpf_printk("Free Ram: %u / %u\n", free_ram_val, total_ram_val);
-
+    bpf_map_delete_elem(&sysinfo_map, &pid);
     return 0;
 }
 
