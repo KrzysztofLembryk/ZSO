07dd40a3de62dae761a01a9c34d1795ed0183908
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2026-05-25 13:39:31 +0000

    Kbuild and some starting code in tapedev.c

---
+++ duze_projekty/3_duze_zad_tape_device/tape_dev/tapedev.c
@@ -0,0 +1,15 @@
+
+int init_tapedev(void)
+{
+
+}
+
+void cleanup_tapedev(void)
+{
+
+}
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Krzysztof Lembryk");
+MODULE_DESCRIPTION("Driver for tapedevice");
